6ea25e447ea8418e23d4199cdd08477b8fde914d
Ilknur <ilknurkazimov308@gmail.com>
2026-02-16 17:21:40 +0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,11 +1,30 @@
 #include "shell.h"
 
-/**
- * main - entry point
- *
- * Return: 0
- */
 int main(void)
 {
-	return (0);
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while (1)
+    {
+        print_promt();
+
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+        {
+            free(line);
+            exit(0);
+        }
+        
+        remove_newline(line);
+
+        if (line[0] == "\0")
+            continue;
+        
+        execute_command(line);
+    }
+
+    return (0);
 }
+
