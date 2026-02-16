62debea0359039d494a4ff0d9527615438173560
Ilknur <ilknurkazimov308@gmail.com>
2026-02-16 16:51:33 +0400

    Update utils.c

---
+++ utils.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+void remove_newline(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        if (str[i] == "\n")
+        {
+            str[i] == "\0";
+            return;
+        }
+        i++;
+    }
+}
