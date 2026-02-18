0d6b93d58ce013d4d0028ae5e1581fd33847051b
Ilknur <ilknurkazimov308@gmail.com>
2026-02-18 09:43:56 +0000

    Update utils.c, add split_args function

--- utils.c
+++ utils.c
@@ -21,3 +21,41 @@ void trim_inplace(char *s)
     if (start > 0)
         memmove(s, s + start, len - start + 1);
 }
+
+
+char **split_args(char *s)
+{
+    char *token;
+    char **argv = NULL;
+    size_t bufsize = 8;
+    size_t idx = 0;
+
+    if (s == NULL)
+        return;
+
+    argv = malloc(bufsize * sizeof(char *));
+    if (argv == NULL)
+    {
+        perror("malloc")
+        return NULL;
+    }
+
+    token = strtok(s, " \t");
+    while (token != NULL)
+    {
+        argv[idx++] = token;
+        if (idx >= bufsize)
+        {
+            bufsize *= 2;
+            argv = realloc(argv, bufsize * sizeof(char *));
+            if (argv == NULL)
+            {
+                perror("realloc");
+                return NULL;
+            }
+        }
+        token = strtok(NULL, " \t");
+    }
+    argv[idx] = NULL;
+    return argv;
+}
