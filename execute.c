3c2751188352bb123b28770553f934ec87be1ed1
Ilknur <ilknurkazimov308@gmail.com>
2026-02-18 09:55:25 +0000

    Update execute.c

--- execute.c
+++ execute.c
@@ -3,29 +3,39 @@
 void execute_command(char *command)
 {
     pid_t pid;
-    char *argv[2];
+    int status;
+    char **argv;
 
-    if (command == NULL || command[0] == '\0')
+    if (command == NULL)
         return;
 
-    argv[0] = command;
-    argv[1] = NULL;
+    argv = split_args(command);
+    if (argv == NULL)
+        return;
+
+    if (argv[0] == NULL)
+    {
+        free(argv);
+        return;
+    }
 
     pid = fork();
     if (pid == -1)
     {
         perror("Error");
+        free(argv);
         return;
     }
 
     if (pid == 0)
     {
-        if (execve(command, argv, environ) == -1)
-            perror(command);
-        _exit(EXIT_FAILURE);
+        execve(argv[0], argv, environ);
+        perror(argv[0]);
+        _exit(127);
     }
     else
     {
-        wait(NULL);
+        waitpid(pid, &status, 0);
+        free(argv);
     }
 }
