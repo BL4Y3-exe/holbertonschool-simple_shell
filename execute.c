3d565e0f37e2ba762199db5a1032fb22cd3ea3d6
Ilknur <ilknurkazimov308@gmail.com>
2026-02-20 13:43:21 +0000

    Upgrade execute.c

--- execute.c
+++ execute.c
@@ -8,22 +8,28 @@ int execute_command(char *command, char *prog_name, int line_number)
     char *full_path;
 
     if (command == NULL)
-        return;
+        return (0);
 
     argv = split_args(command);
-    if (argv == NULL || argv[0] == NULL)
+    if (argv == NULL)
+        return (0);
+
+    if (argv[0] == NULL)
     {
         free(argv);
-        return;
+        return (0);
     }
 
     full_path = find_in_path(argv[0]);
     if (full_path == NULL)
     {
         fprintf(stderr, "%s: %d: %s: not found\n",
-        prog_name, line_number, argv[0]);
+                prog_name, line_number, argv[0]);
         free(argv);
-        exit(127);
+
+        if (!isatty(STDIN_FILENO))
+            return (127);
+        return (0);
     }
 
     pid = fork();
@@ -32,19 +38,21 @@ int execute_command(char *command, char *prog_name, int line_number)
         perror("fork");
         free(argv);
         free(full_path);
-        return;
+        return (0);
     }
 
     if (pid == 0)
     {
         execve(full_path, argv, environ);
         perror(argv[0]);
-        exit(127);
+        _exit(127);
     }
     else
     {
         waitpid(pid, &status, 0);
     }
+
     free(full_path);
     free(argv);
+    return (0);
 }
